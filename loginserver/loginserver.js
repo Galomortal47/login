32fb0dd19a372288815328ad9c85cc0520c8e49f
unknown <galomortalbrgames@gmail.com>
2020-10-18 03:34:41 -0300

    add

--- loginserver/loginserver.js
+++ loginserver/loginserver.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var app = express();
+var database = require("./mongodb.js");
+var db = new database();
+
+const userDataSchema = {
+	// _id: Number,
+	username: "",
+	user_password: "",
+	email: "",
+  salt: "",
+	content: {}
+};
 
 // on the request to root (localhost:3000/)
 app.get('*', function (req, res) {
-    console.log(req.originalUrl);
-    res.send('{"message":123}');
+    //console.log(req.originalUrl);
+    var str = req.originalUrl.toString()
+    var jsonObj = JSON.parse(str.slice(1,str.length));
+    userDataSchema.username = jsonObj.username
+    userDataSchema.user_password = jsonObj.hash_and_salt
+    userDataSchema.email = jsonObj.email
+    userDataSchema.salt = jsonObj.salt
+    var error = db.create(userDataSchema);
+    //console.log(error);
+    if(!(error == null)){
+      res.send('{"response": "Username Already Taken"}');
+      console.log("Username Already Taken")
+    }else{
+      res.send('{"response": "Registered Succesfully"}');
+      console.log("Registered Succesfully")
+      }
 });
 
 // Change the 404 message modifing the middleware
--- loginsystem/Register.gd
+++ loginsystem/Register.gd
@@ -1,16 +1,49 @@
 extends Control
 
+onready var login = get_parent().get_node("Login")
+onready var http = get_parent().get_node("HTTPRequest")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_login_button_down():
+	hide()
+	login.show()
 	pass # Replace with function body.
 
+var data = {
+"username":"",
+"email":"",
+"password":"",
+"password2":"",
+"salt":"",
+"hash_and_salt":"",
+"operation":""
+}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_register_button_down():
+	data.username = get_node("username").get_text()
+	data.email = get_node("email").get_text()
+	data.password = get_node("password").get_text()
+	data.password2 = get_node("password2").get_text()
+	data.operation = "register"
+	randomize()
+	data.salt = str(int(rand_range(10000000,99999999)))
+	if data.password != data.password2:
+		get_node("AcceptDialog").set_text('password doesent match')
+		get_node("AcceptDialog").popup_centered(Vector2(180,60))
+		get_node("AcceptDialog").show()
+		return
+	if data.password.length() < 3:
+		get_node("AcceptDialog").set_text('password is too short the minimum is 8 digits')
+		get_node("AcceptDialog").popup_centered(Vector2(150,60))
+		get_node("AcceptDialog").show()
+		return
+	if data.username.length() < 1 or data.email.length() < 1:
+		get_node("AcceptDialog").set_text('username and email are obligatory')
+		get_node("AcceptDialog").popup_centered(Vector2(150,60))
+		get_node("AcceptDialog").show()
+	var hasher = data.password.sha256_text()
+	var salt = data.salt.sha256_text()
+	data.hash_and_salt = (hasher+salt).sha256_text()
+	data.password = ""
+	data.password2 = ""
+	http._send_data(data)
+	pass # Replace with function body.
